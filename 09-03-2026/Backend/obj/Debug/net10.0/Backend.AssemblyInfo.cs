2a4e47888dfeb114e6afdbb0e5cd84f8fe4ef3da
AayushMathur03 <mathuraayush03@gmail.com>
2026-03-09 15:18:22 +0530

    add: 5 tasks done arrow fns

--- 09-03-2026/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ 09-03-2026/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bd2febc1a0cb2bdb72c71b69eecef6d6eb299ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6571b803fc88b9f7948717a43568fb999cb87f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
